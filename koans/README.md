644ed8a88e3e1365d709ee2ab0019bd9ee812c7e
mcook42 <mattheworion.cook@gmail.com>
2023-01-14 16:13:59 -0700

    initial commit

---
+++ koans/README.md
@@ -0,0 +1,10 @@
+# Ruby Learning Materials
+
+This repo houses my Ruby learning materials.  The first commit for each subdirectory will be the original files provided by the educational resource. The following commits show my progress.
+
+## Setup
+run `. setup.sh` in the root directory to install the necessary software
+
+## Tech Used
+- VSCode (for code editing)
+- rbenv (for version management)
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/urs/bin/env bash
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'eval "$(~/.rbenv/bin/rbenv init - bash)"' >> ~/.bashrc
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+# https://github.com/rbenv/ruby-build/wiki#ubuntudebianmint 
+apt-get install autoconf bison patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
