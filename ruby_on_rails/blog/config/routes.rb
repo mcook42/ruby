5c0d7f926289533c458c8e345b3c67a5670b0060
mcook42 <mattheworion.cook@gmail.com>
2023-01-22 10:11:39 -0700

    moved to :resources to provide default CRUD abilities to the Articles route

--- ruby_on_rails/blog/config/routes.rb
+++ ruby_on_rails/blog/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show" # ArticlesController can now access the id value provided via `params[:id]`
-
+  resources :articles
 end
